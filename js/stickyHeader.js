ef9895a1c65c57fc1d86d2ebafe961d152f7fd40
Rupert Foley <rupert.foley@gmail.com>
2021-09-27 13:10:09 +0100

    fixed header javascript

--- js/stickyHeader.js
+++ js/stickyHeader.js
@@ -1,28 +1,25 @@
 const header = document.getElementById("header");
 const inner = document.getElementById("inner");
-let sticky = 200;
+let sticky = 100;
 let lastScroll = 0;
 
 function stickyHeader(){
     if ( cntr.scrollTop < lastScroll ){
         header.classList.add("sticky-show");
-        header.classList.remove("sticky-hide");
-        inner.style.marginTop = "106px";
+        header.classList.remove("sticky-hide"); 
     }
     else {
         header.classList.remove("sticky-show");
         header.classList.add("sticky-hide");
-        inner.style.marginTop = "0";
     }
     lastScroll = cntr.scrollTop;
 }
-console.log("they see me scrollin")
-cntr.onscroll = function(){ 
-    console.log("they see me scrollin")
-    if ( cntr.scrollTop > sticky ){
 
+cntr.onscroll = function(){ 
+    if ( cntr.scrollTop > sticky ){
         header.classList.add("sticky");
         stickyHeader();
+        inner.style.marginTop = "86px";
     }
     else if(cntr.scrollTop == 0){
         header.classList.remove("sticky");
